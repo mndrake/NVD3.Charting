e961cba224bcfc78a59741a75b4d584a95f4aba6
David Carlson <dacarlson@mac.com>
2015-02-03 06:20:52 -0600

    initial commit

---
+++ NVD3.Charting/Chart.fs
@@ -0,0 +1,70 @@
+﻿module NVD3.Charting
+open System
+open Newtonsoft.Json
+
+type Point = {x: float; y:float}
+type Series = {values: Point seq; key: string; color: string}
+type Axis = {label: string option; format: string option}
+
+let template = """
+<svg id="{GUID}" style='height:{HEIGHT}px;width:{WIDTH}px'></svg>
+<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/nvd3/1.1.15-beta/nv.d3.min.css">
+<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/require.js/2.1.15/require.min.js"></script>
+<script type="text/javascript">
+    require.config({
+        paths: {d3: "//cdnjs.cloudflare.com/ajax/libs/d3/3.5.3/d3.min",
+                nvd3: "//cdnjs.cloudflare.com/ajax/libs/nvd3/1.1.15-beta/nv.d3.min"}});
+    require(["d3", "nvd3"], function(d3, nvd3) {
+                nv.addGraph(function() {
+                    var chart = nv.models.lineChart().useInteractiveGuideline(true);
+                    {XAXIS}
+                    {YAXIS}
+                    d3.select('#{GUID}')
+                            .datum({DATA})
+                            .transition().duration(500)
+                            .call(chart);
+                    nv.utils.windowResize(chart.update);
+                    return chart;});});
+</script>"""
+
+type NChart = 
+    {
+        guid: string
+        xAxis: Axis option
+        yAxis: Axis option
+        height: int
+        width: int
+        datum: Series seq 
+    }
+
+    with  
+
+    static member Line(values: seq<float*float>) = 
+        { guid = "N" + System.Guid.NewGuid().ToString("N")
+          xAxis = Some {label = None; format = Some "0.1f"}
+          yAxis = Some {label = None; format = Some "0.1f"}
+          height = 400
+          width = 600
+          datum = 
+              [{ values = values |> Seq.map (fun p -> {x = fst p; y = snd p }) 
+                 key = "Series 1"
+                 color = "#ff7f0e" }]
+         }
+
+    member __.toHtml() =
+        let axisFormat (axis:Axis) name =
+            let label = match axis.label with
+                        | Some v -> String.Format(".axisLabel('{0}'", v)
+                        | None   -> ""
+            let tickFormat = match axis.format with
+                             | Some v -> String.Format(".tickFormat(d3.format('{0}'))", v)
+                             | None   -> ""
+            String.Format("chart.{0}{1}{2}", name, label, tickFormat)
+        let xAxis = match __.xAxis with |None -> "" |Some v -> "xAxis" |> axisFormat v
+        let yAxis = match __.yAxis with |None -> "" |Some v -> "yAxis" |> axisFormat v
+        template.Replace("{GUID}", __.guid)
+            .Replace("{DATA}", (__.datum |> JsonConvert.SerializeObject))
+            .Replace("{WIDTH}", string __.width)
+            .Replace("{HEIGHT}", string __.height)
+            .Replace("{XAXIS}", xAxis)
+            .Replace("{YAXIS}", yAxis)
---
+++ NVD3.Charting/Script.fsx
@@ -0,0 +1,15 @@
+﻿// Learn more about F# at http://fsharp.net. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+#r "bin/Debug/NVD3.Charting"
+#r "bin/Debug/Newtonsoft.Json"
+
+open NVD3.Charting
+open Newtonsoft.Json
+
+let data = [| for i in 0. .. 10. -> i, i*i |]
+
+let json = 
+    data
+    |> Array.map (fun p -> { x = fst p; y = snd p })
+    |> JsonConvert.SerializeObject
